5870b9d72b7ce1437ba448caa899b90088160f7f
Julien Blanchard <julien@sideburns.eu>
2015-01-02 19:45:23 +0100

    HW1 EX5

--- homework1.hs
+++ homework1.hs
@@ -20,3 +20,11 @@ sumDigits = foldr ((+) . sum . toDigits) 0
 
 validate :: Integer -> Bool
 validate n = sumDigits(doubleEveryOther $ toDigitsRev n) `mod` 10 == 0
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
