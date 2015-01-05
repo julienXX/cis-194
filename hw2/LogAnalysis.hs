cc19a74ccec967cca335e53f931d4611d89d077f
Julien Blanchard <julien@sideburns.eu>
2015-01-05 11:17:57 +0100

    Base LogAnalysis

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -1,1 +1,15 @@
+{-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
+
+import Log
+
+data MessageType = Info
+                 | Warning
+                 | Error Int
+                 deriving (Show, Eq)
+
+type TimeStamp = Int
+
+data LogMessage = LogMessage MessageType TimeStamp String
+                | Unknown String
+                deriving (Show, Eq)
