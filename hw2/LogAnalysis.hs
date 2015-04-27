059e02533fdb5df7e0102670f05936255ec38b5b
Julien Blanchard <julien@sideburns.eu>
2015-04-27 15:59:24 +0200

    HW2EX1

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -1,15 +1,25 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
 module LogAnalysis where
 
 import Log
+import Data.Char (isSpace)
 
-data MessageType = Info
-                 | Warning
-                 | Error Int
-                 deriving (Show, Eq)
+parseRest :: String -> (Int, String)
+parseRest string =
+  let [(t, rest)] = lex string in
+   (read t :: Int, dropWhile isSpace rest)
 
-type TimeStamp = Int
+parseMessage :: String -> LogMessage
+parseMessage message =
+  let [(severity, rest)] = lex message
+  in
+   case severity of
+    "I" -> let (ts, msg) = parseRest rest in LogMessage Info ts msg
+    "W" -> let (ts, msg) = parseRest rest in LogMessage Warning ts msg
+    "E" -> let (e, rest') = parseRest rest
+           in
+            let (ts, msg) = parseRest rest' in LogMessage (Error e) ts msg
 
-data LogMessage = LogMessage MessageType TimeStamp String
-                | Unknown String
-                deriving (Show, Eq)
+parse :: String -> [LogMessage]
+parse = (map parseMessage) . lines
